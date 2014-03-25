f4231eace3b99f5e31cf01da4ad2e5337f5d58fa
poppe1219 <robin.asen@gmail.com>
2014-03-25 10:35:59 +0100

    Adjusted mouse parser to handle negative coordinates.

--- util/test_proxy_actions.py
+++ util/test_proxy_actions.py
@@ -64,6 +64,12 @@ class TestActions(unittest.TestCase):
     ProxyMouse("<7 9>").execute()
     comm.execute_batch.assert_called_with([('move_mouse', (), {'x': 7.0, 'y': 9.0, 'proportional': False, 'reference': 'relative'})])
 
+    ProxyMouse("<7 -9>").execute()
+    comm.execute_batch.assert_called_with([('move_mouse', (), {'x': 7.0, 'y': -9.0, 'proportional': False, 'reference': 'relative'})])
+
+    ProxyMouse("<-7 9>").execute()
+    comm.execute_batch.assert_called_with([('move_mouse', (), {'x': -7.0, 'y': 9.0, 'proportional': False, 'reference': 'relative'})])
+
     ProxyMouse("<-7 -9>").execute()
     comm.execute_batch.assert_called_with([('move_mouse', (), {'x': -7.0, 'y': -9.0, 'proportional': False, 'reference': 'relative'})])
 

35d61630627fc234e495055fe3c2ab2c6083e092
poppe1219 <robin.asen@gmail.com>
2014-04-19 18:00:22 +0200

    Added a hack to jsonrpclib/xmlrpclib to reduce connection timeout.

--- util/communications.py
+++ util/communications.py
@@ -1,11 +1,31 @@
 import jsonrpclib
+import xmlrpclib
+import httplib
 
 import config
 
+
+# Override of the xmlrpclib.Transport class to change the connection timeout.
+class Transport(jsonrpclib.jsonrpc.TransportMixIn, xmlrpclib.Transport):
+    def make_connection(self, host):
+        #return an existing connection if possible.  This allows
+        #HTTP/1.1 keep-alive.
+        if self._connection and host == self._connection[0]:
+            return self._connection[1]
+
+        # create a HTTP connection object from a host descriptor
+        chost, self._extra_headers, x509 = self.get_host_info(host)
+        #store the host argument along with the connection object
+        self._connection = host, httplib.HTTPConnection(chost, timeout=1)
+        return self._connection[1]
+
+
 class Proxy(object):
   def __init__(self, host, port):
-    self.server = jsonrpclib.Server("http://%s:%i" % (host, port))
-    self._server = jsonrpclib.Server("http://%s:%i" % (host, 8888))  # develop
+    self.server = jsonrpclib.Server("http://%s:%i" % (host, port),
+        transport=Transport())  # Use the custom Transport class.
+    self._server = jsonrpclib.Server("http://%s:%i" % (host, 8888),
+        transport=Transport())  # Use the custom Transport class.
 
   def toggle_server(self):
       temp = self.server
